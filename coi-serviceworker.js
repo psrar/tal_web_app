f86cd2a1f47c367875ff70b721c2dcd2e64018d0
Pavel Simonian <pavel.sima203@gmail.com>
2026-04-28 10:25:25 +0300

    0.1.0

---
+++ coi-serviceworker.js
@@ -0,0 +1,41 @@
+const coi = {
+  shouldRegister: () => true,
+  shouldDeregister: () => false,
+  doNotCheck: () => false,
+  quiet: false
+};
+
+if (typeof window === "undefined") {
+  self.addEventListener("install", () => self.skipWaiting());
+  self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
+
+  self.addEventListener("fetch", (event) => {
+    if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") {
+      return;
+    }
+
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          if (response.status === 0) {
+            return response;
+          }
+
+          const newHeaders = new Headers(response.headers);
+          newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
+          newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
+
+          return new Response(response.body, {
+            status: response.status,
+            statusText: response.statusText,
+            headers: newHeaders,
+          });
+        })
+        .catch((e) => console.error(e))
+    );
+  });
+} else {
+  const script = document.createElement("script");
+  script.src = "coi-serviceworker.js";
+  document.head.appendChild(script);
+}
