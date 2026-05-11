f573ca0635f035feba56b6cf2d87e978210b3a8d
Pavel Simonian <pavel.sima203@gmail.com>
2026-05-11 16:59:32 +0300

    0.1.9

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4051493999" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "303280948" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
